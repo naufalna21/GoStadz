37a1d2a58a398b91f4e7e73a42ced1bc628f9880
naufalnabbih <naufalnabbih@gmail.com>
2023-01-07 14:45:08 +0700

    Login_Start

---
+++ app/src/main/java/com/naufal/gostadzfix/StartScreen.java
@@ -0,0 +1,27 @@
+package com.naufal.gostadzfix;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class StartScreen extends AppCompatActivity {
+    private Button b_useemail;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.startscreen);
+        b_useemail = findViewById(R.id.useemail);
+        b_useemail.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                LoginScreen(view);
+            }
+        });
+    }
+    public void LoginScreen (View view){
+        startActivity(new Intent(this, LoginScreen.class));
+    }
+}
